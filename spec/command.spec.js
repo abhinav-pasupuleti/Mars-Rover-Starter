14e1474468c424592476e3ece313743d95fd0edc
abhi pasupuleti <abhi25.pasupuleti@gmail.com>
2024-08-28 12:16:38 -0500

    Trying to see why autograder failed

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -4,7 +4,7 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Command class", function () {
-
+  //3 test cases
   it("throws error if command type is NOT passed into constructor as the first parameter", function () {
     expect(function () { 
       new Command();
